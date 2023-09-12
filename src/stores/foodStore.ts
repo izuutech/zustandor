d31ca8cb32cef52935e38e4dfa5a64751c6d3c45
izuutech <izutechsofficial@gmail.com>
2023-09-12 23:47:11 +0100

    ss

---
+++ src/stores/foodStore.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type TFoodStoreState = {
+  fish: number;
+  addOneFish: () => void;
+  removeOneFish: () => void;
+  removeAllFish: () => void;
+};
+export const useFoodStore = create<TFoodStoreState>()((set) => ({
+  fish: 0,
+  addOneFish: () => set((state) => ({ fish: state.fish + 1 })),
+  removeOneFish: () => set((state) => ({ fish: state.fish - 1 })),
+  removeAllFish: () => set({ fish: 0 }),
+}));
